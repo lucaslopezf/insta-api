59b191ae39dbe66ba2533e0b8af02eb0c3405152
brs_leonel <leonel@brightsector.com>
2020-11-10 00:33:02 -0300

    Cambia a version 2.0.0, cambiando compatibilidad. Se agregan varios pedidos: like, save_post, follow, unfollow. Cambia estructura de pedidos anteriores

--- src/components/post/routes.ts
+++ src/components/post/routes.ts
@@ -1,14 +1,64 @@
 import { Request, Response } from 'express';
-import { publicComment } from './service';
+import { publicComment, likePost, makeAuth, followUser, unfollowUser, savePost } from './service';
 import { HttpStatusCode } from '../../commons/constants';
 
 export default [
   {
-    path: '/post/',
+    path: '/make_comments/',
     method: 'post',
     handler: [
       async (req: Request, res: Response): Promise<void> => {
         await publicComment(req.body);
+        res.status(HttpStatusCode.Ok).send('ok');
+      },
+    ],
+  },
+  {
+    path: '/save_post/',
+    method: 'post',
+    handler: [
+      async (req: Request, res: Response): Promise<void> => {
+        await savePost(req.body);
+        res.status(HttpStatusCode.Ok).send('ok');
+      },
+    ],
+  },
+  {
+    path: '/logged/',
+    method: 'post',
+    handler: [
+      async (req: Request, res: Response): Promise<void> => {
+        await makeAuth();
+        res.status(HttpStatusCode.Ok).send('ok');
+      },
+    ],
+  },
+  {
+    path: '/like/',
+    method: 'post',
+    handler: [
+      async (req: Request, res: Response): Promise<void> => {
+        await likePost(req.body);
+        res.status(HttpStatusCode.Ok).send('ok');
+      },
+    ],
+  },
+  {
+    path: '/follow/',
+    method: 'post',
+    handler: [
+      async (req: Request, res: Response): Promise<void> => {
+        await followUser(req.body);
+        res.status(HttpStatusCode.Ok).send('ok');
+      },
+    ],
+  },
+  {
+    path: '/unfollow/',
+    method: 'post',
+    handler: [
+      async (req: Request, res: Response): Promise<void> => {
+        await unfollowUser(req.body);
         res.status(HttpStatusCode.Ok).send('ok');
       },
     ],
--- src/components/post/service.ts
+++ src/components/post/service.ts
@@ -1,41 +1,56 @@
-import { HTTP400Error,logger,random,sleep } from '../../utils';
+import { HTTP400Error,logger,random } from '../../utils';
 import { getPage,authenticate } from '../puppeteer/service';
-import { separator,textTareaComment,buttonSubmit,buttonErrorComment} from '../../commons/constants';
+import { textAreaComment,buttonSubmit,buttonErrorComment,buttonLike,buttonFollowFromPost,buttonSavePost,buttonFollowingFromPost,buttonUnfollow} from '../../commons/constants';
 import { Post,ErrorComment } from './models';
 import { Page } from 'puppeteer';
-import { permutations } from './combination';
 
-const makePermutationsComments = (users:string[], quantity:number) : string[] => {
-  let comments: string[] = quantity == 1 ? users : permutations(users,quantity-1);
-  return comments.sort(() => Math.random() - 0.5);
+
+export const publicComment = async ({url, customizableComments}: Post): Promise<void> => {
+  const page = await getPage(url);
+  logger.info(`Total comments to post: ${customizableComments.length}`);
+  for (const comment of customizableComments) await commentPost(page,comment);
+};
+
+export async function makeAuth(): Promise<void> {
+  const isAuth = await authenticate();
+  if(!isAuth) throw new HTTP400Error("Inccorect username or password");
 }
 
-export const publicComment = async ({url,quantity,comment : { customizableComments,users,hashtag},unique}: Post): Promise<void> => {
-  
-  const commentsPost = customizableComments ?? makePermutationsComments(users,quantity);
-  
-  if (!isInstagram(url)) throw new HTTP400Error("Enter an instagram link");
-  const isAuth = await authenticate();
-  
-  if(!isAuth) throw new HTTP400Error("Inccorect username or password");
-  
+export async function likePost ({url} :Post): Promise<void>  {
   const page = await getPage(url);
-  logger.info(`Total comments to post: ${commentsPost.length}`);
-  for (const comment of commentsPost) await commentPost(page,comment,hashtag,unique);
-};
+  logger.info(`Like to: ${url}`);
+  page.click(buttonLike); 
+  await page.waitFor(2000);
+}
+
+export async function followUser ({url} :Post): Promise<void>  {
+  const page = await getPage(url);
+  logger.info(`Follow the url: ${url}`);
+  page.click(buttonFollowFromPost);
+  await page.waitFor(2000);
+}
+
+export async function savePost ({url} :Post): Promise<void>  {
+  const page = await getPage(url);
+  logger.info(`Save the url: ${url}`);
+  page.click(buttonSavePost);
+  await page.waitFor(2000);
+}
+
+export async function unfollowUser ({url} :Post): Promise<void>  {
+  const page = await getPage(url);
+  logger.info(`Unfollow the url: ${url}`);
+  page.click(buttonFollowingFromPost); 
+  await page.waitFor(2000);
+  page.click(buttonUnfollow); 
+  await page.waitFor(2000);
+}
 
 let commentCounter = 0;
 let errorCounter = 0;
-const commentPost = async (page: Page,message:string,hashtag: string = '',unique:Boolean = true): Promise<void> => {
-  //TODO: Poorly performance, refactor this 
-  if(unique){
-    const messageSplit = message.split(separator);
-    let uniqueMessage = [...new Set(messageSplit)]; 
-    if(uniqueMessage.length < messageSplit.length) return;
-  }
-  
+const commentPost = async (page: Page,message:string): Promise<void> => {
   const breakBot = 3;
-  const comment = `${hashtag} ${message}`;
+  const comment = `${message}`;
   const timeWaitTypeComment = random(125,1324);
   const timeWaitBetweenComments = random(timeWaitTypeComment+5217,32542);
 
@@ -48,16 +63,11 @@ const commentPost = async (page: Page,message:string,hashtag: string = '',unique
   await page.waitFor(timeWaitBetweenComments);
 };
 
-const isInstagram = (url:string): boolean => {
-    //TODO: Implementar
-    return true;
-};
-
 const writeComment = async (comment:string,page:Page,timeWaitTypeComment:number = 111111): Promise<boolean> => {
   let result = true;
 
-  await page.waitForSelector(textTareaComment);
-  await page.type(textTareaComment, comment,{ delay: timeWaitTypeComment });
+  await page.waitForSelector(textAreaComment);
+  await page.type(textAreaComment, comment,{ delay: timeWaitTypeComment });
   
   await page.click(buttonSubmit);
   
